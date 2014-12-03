cd4674b02dc05b59043ea2fcaccd84a4b61c45c4
Andrew Barrett <bazerk@gmail.com>
2014-12-04 11:00:22 +1300

    Initial cut of algorithm

--- ukmodulus/__init__.py
+++ ukmodulus/__init__.py
@@ -19,27 +19,32 @@ ModulusWeight = namedtuple('ModulusWeight',
                             'algorithm',
                             'weightings',
                             'exception_code'])
+
+POSITION_U = 0
+POSITION_V = 1
+POSITION_W = 2
+POSITION_X = 3
+POSITION_Y = 4
+POSITION_Z = 5
+POSITION_A = 6
+POSITION_B = 7
+POSITION_C = 8
+POSITION_D = 9
+POSITION_E = 10
+POSITION_F = 11
+POSITION_G = 12
+POSITION_H = 13
+
+DBLAL = 'DBLAL'
+MOD10 = 'MOD10'
+MOD11 = 'MOD11'
+
+_sc_subs = {}
 _weightings = []
-_positions = {
-    'u': 0,
-    'v': 1,
-    'w': 2,
-    'x': 3,
-    'y': 4,
-    'z': 5,
-    'a': 6,
-    'b': 7,
-    'c': 8,
-    'd': 9,
-    'e': 10,
-    'f': 11,
-    'g': 12,
-    'h': 13,
-}
 _mod_values = {
-    'DBLAL': 10,
-    'MOD10': 10,
-    'MOD11': 11,
+    DBLAL: 10,
+    MOD10: 10,
+    MOD11: 11,
 }
 
 
@@ -63,18 +68,87 @@ def _get_weightings(sort_code):
     return weightings
 
 
-def _run_check(combined, weighting):
-    mod_value = _mod_values.get(weighting.algorithm, None)
-    if mod_value is None:
-        raise ValueError('Unknown algorithm %s' % weighting.algorithm)
+def _combine_sort_and_account(sort_code, account_number, weighting):
+    if weighting.exception_code == '5' and weighting.algorithm != 'DBLAL' and sort_code in _sc_subs:
+        sort_code = _sc_subs[sort_code]
+    elif weighting.exception_code == '8':
+        sort_code = '090126'
+    elif weighting.exception_code == '9':
+        sort_code = '309634'
+    combined = sort_code + account_number
+    return combined
+
+
+def _skip_check(weighting, combined):
+    if weighting.exception_code == '3':
+        if combined[POSITION_C] == '6' or combined[POSITION_C] == '9':
+            return True
+    elif weighting.exception_code == '6':
+        if combined[POSITION_A] in '45678' and combined[POSITION_G] == combined[POSITION_H]:
+            return True
+    return False
+
+
+def _get_weighting_to_use(weighting, combined):
+    weightings = weighting.weightings
+    if weighting.exception_code == '2':
+        if combined[POSITION_A] != '0' and combined[POSITION_G] != '9':
+            weightings = [0, 0, 1, 2, 5, 3, 6, 4, 8, 7, 10, 9, 3, 1]
+        elif combined[POSITION_A] != '0' and combined[POSITION_G] == '9':
+            weightings = [0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 10, 9, 3, 1]
+    elif weighting.exception_code == '7':
+        if combined[POSITION_G] == '9':
+            weightings = list(weightings)
+            for ix in range(POSITION_B + 1):
+                weightings[ix] = 0
+    elif weighting.exception_code == '10':
+        ab = combined[POSITION_A] + combined[POSITION_B]
+        if ab in ('09', '99') and combined[POSITION_G] == '9':
+            weightings = list(weightings)
+            for ix in range(POSITION_B + 1):
+                weightings[ix] = 0
+    return weightings
+
+
+def _exception_5_remainder_check(algorithm, combined, remainder):
+    if algorithm == DBLAL:
+        if remainder == 0 and combined[POSITION_H] == 0:
+            return True
+        else:
+            check = 10 - remainder
+            return check == combined[POSITION_H]
+    else:
+        if remainder == 1:
+            return False
+        if remainder == 0 and combined[POSITION_G] == 0:
+            return True
+        check = 11 - remainder
+        return check == combined[POSITION_G]
+
+
+def _run_check(sort_code, account_number, weighting):
+    combined = _combine_sort_and_account(sort_code, account_number, weighting)
+    if _skip_check(weighting, combined):
+        return True
+    weightings = _get_weighting_to_use(weighting, combined)
     total = 0
     for ix, ch in enumerate(combined):
-        value = int(ch) * weighting.weightings[ix]
-        if weighting.algorithm == 'DBLAL':
+        value = int(ch) * weightings[ix]
+        if weighting.algorithm == DBLAL:
             total += sum(int(x) for x in str(value))
         else:
             total += value
-    return (total % mod_value) == 0
+    if weighting.exception_code == '1':
+        total += 27
+    mod_value = _mod_values.get(weighting.algorithm, None)
+    remainder = total % mod_value
+    if weighting.exception_code == '5':
+        return _exception_5_remainder_check(weighting.algorithm, combined, remainder)
+    if weighting.exception_code == '4':
+        checkval = int(combined[POSITION_G] + combined[POSITION_H])
+    else:
+        checkval = 0
+    return remainder == checkval
 
 
 def validate_number(sort_code, account_number):
@@ -84,11 +158,23 @@ def validate_number(sort_code, account_number):
     sort_code = _clean_input(sort_code, required_length=6)
     account_number = _clean_input(account_number, required_length=8)
     weightings = _get_weightings(sort_code)
+    check_count = len(_weightings)
     if not weightings:
         return True
-    combined = sort_code + account_number
-    # for weighting in weightings:
-    return True
+    first_check = _weightings[0]
+    if _run_check(sort_code, account_number, first_check):
+        if check_count == 1 or first_check.exception_code in ('2', '9', '10', '11', '12', '13', '14'):
+            return True
+        return _run_check(sort_code, account_number, _weightings[1])
+    else:
+        if first_check.exception_code == '14':
+            if account_number[7] not in '019':
+                return False
+            account_number = ('0' + account_number)[:8]
+            return _run_check(sort_code, account_number, _weightings[0])
+        if check_count == 1:
+            return False
+        return _run_check(sort_code, account_number, _weightings[1])
 
 
 def _load_weightings():
@@ -107,4 +193,17 @@ def _load_weightings():
                                       split[17] if column_count == 18 else None)
             _weightings.append(weighting)
 
+
+def _load_scsubs():
+    with open(os.path.join(os.path.dirname(__file__), 'scsubtab.txt'), 'r') as f:
+        for line in f.readlines():
+            if not line:
+                break
+            split = line.split()
+            if len(split) != 2:
+                raise ValueError
+            _sc_subs[split[0]] = split[1]
+
+
 _load_weightings()
+_load_scsubs()
--- ukmodulus/tests.py
+++ ukmodulus/tests.py
@@ -1,6 +1,6 @@
 import unittest
 # noinspection PyProtectedMember
-from ukmodulus import validate_number, _clean_input, _get_weightings, ModulusWeight, _run_check
+from ukmodulus import validate_number, _clean_input, _get_weightings, ModulusWeight, _run_check, _sc_subs
 
 
 class SimpleValidationTests(unittest.TestCase):
@@ -15,16 +15,78 @@ class SimpleValidationTests(unittest.TestCase):
 class RunAlgorithmChecks(unittest.TestCase):
     def test_dbla(self):
         cases = [
-            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], None), '49927312345678', False),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], None), '499273', '12345678',
+             True),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], None), '499273', '12345679',
+             False),
         ]
         for case in cases:
-            self.assertEqual(_run_check(case[1], case[0]), case[2])
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_mod10(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD10', [0, 0, 0, 0, 0, 0, 7, 1, 3, 7, 1, 3, 7, 1], None), '080211', '11111111',
+             True),
+            (ModulusWeight('', '', 'MOD10', [0, 0, 0, 0, 0, 0, 7, 1, 3, 7, 1, 3, 7, 1], None), '080211', '11111112',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_mod11(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 7, 5, 8, 3, 4, 6, 2, 1], None), '000000', '58177632',
+             True),
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 7, 5, 8, 3, 4, 6, 2, 1], None), '000000', '58177633',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_1(self):
+        cases = [
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '1'), '499273', '12345678',
+             False),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '1'), '499273', '12345671',
+             True),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_3(self):
+        cases = [
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '3'), '499273', '12645678',
+             True),
+            (ModulusWeight('', '', 'DBLAL', [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1], '3'), '499273', '12945671',
+             True),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_4(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], '4'), '000000', '00000009',
+             True),
+            (ModulusWeight('', '', 'MOD11', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], '4'), '000000', '00000011',
+             False),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
+
+    def test_exception_5(self):
+        cases = [
+            (ModulusWeight('', '', 'MOD11', [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], '4'), '938173', '00000009',
+             True),
+        ]
+        for case in cases:
+            self.assertEqual(_run_check(case[1], case[2], case[0]), case[3])
 
 
 class ValcadosDataLoadingTests(unittest.TestCase):
     """
     These test the loading of the valacdos.txt data
     """
+
     def test_valid_weightings(self):
         weightings = _get_weightings('010005')
         self.assertEqual(len(weightings), 1)
@@ -49,6 +111,11 @@ class ValcadosDataLoadingTests(unittest.TestCase):
         weightings = _get_weightings('999999')
         self.assertEqual(len(weightings), 0)
 
+    def test_scsubs(self):
+        self.assertEqual(_sc_subs.get('938173', None), '938017')
+        self.assertEqual(_sc_subs.get('938654', None), '938621')
+        self.assertEqual(_sc_subs.get('838654', None), None)
+
 
 class ValacdosTests(unittest.TestCase):
     """
@@ -58,8 +125,8 @@ class ValacdosTests(unittest.TestCase):
     def test_case_no1(self):
         self.assertEqual(validate_number('089999', '66374958'), True)
 
-    # def test_case_no23(self):
-    #     self.assertEqual(validate_number('938063', '15764273'), False)
+        # def test_case_no23(self):
+        # self.assertEqual(validate_number('938063', '15764273'), False)
 
 
 if __name__ == '__main__':
