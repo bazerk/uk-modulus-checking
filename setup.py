a1fc14e02c091a5ec1775ce520b887ab4a00edb5
Andrew Barrett <bazerk@gmail.com>
2014-12-04 11:44:52 +1300

    Add files for pypy

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='ukmodulus',
+    packages=['ukmodulus'],
+    version='0.1',
+    description='Implementation of VocaLinks UK Modulus checking for UK account numbers and sort codes',
+    author='Andrew Barrett',
+    author_email='bazerk+pypy@gmail.com',
+    url='https://github.com/bazerk/uk-modulus-checking',
+    download_url='https://github.com/bazerk/uk-modulus-checking/tarball/0.1',
+    keywords=['uk', 'banking', 'validation'],
+    classifiers=[],
+)
