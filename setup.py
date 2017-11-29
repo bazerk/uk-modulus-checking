16ea81af27f0d020b742daf5d4681b72c64dbf15
Andrew Barrett <bazerk@gmail.com>
2017-11-29 20:25:02 +1300

    Update PyPI release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='ukmodulus',
     packages=['ukmodulus'],
-    version='0.1.1',
+    version='0.1.2',
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
@@ -12,7 +12,7 @@ setup(
     author='Andrew Barrett',
     author_email='bazerk+pypy@gmail.com',
     url='https://github.com/bazerk/uk-modulus-checking',
-    download_url='https://github.com/bazerk/uk-modulus-checking/tarball/0.1',
+    download_url='https://github.com/bazerk/uk-modulus-checking/tarball/0.1.2',
     keywords=['uk', 'banking', 'validation'],
     classifiers=[],
     licence='MIT',
