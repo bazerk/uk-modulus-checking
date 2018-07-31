ab5fa29b81910f2651ef3592b5a34deef687d263
Ivan Markevich <imarkevich@holvi.com>
2018-07-31 11:51:24 +0300

    bump new version for holvi pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
+# override for holvi pypi
 from distutils.core import setup
 
 setup(
     name='ukmodulus',
     packages=['ukmodulus'],
-    version='0.1.2',
+    version='0.1.3',
     package_data={
         # If any package contains *.txt or *.rst files, include them:
         '': ['*.txt', '*.rst'],
@@ -11,8 +12,8 @@ setup(
     description='Implementation of VocaLinks UK Modulus checking for UK account numbers and sort codes',
     author='Andrew Barrett',
     author_email='bazerk+pypy@gmail.com',
-    url='https://github.com/bazerk/uk-modulus-checking',
-    download_url='https://github.com/bazerk/uk-modulus-checking/tarball/0.1.2',
+    url='https://github.com/holvi/uk-modulus-checking',
+    download_url='https://github.com/holvi/uk-modulus-checking/tarball/0.1.3',
     keywords=['uk', 'banking', 'validation'],
     classifiers=[],
     licence='MIT',
